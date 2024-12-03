a8af34093e93a6f27038d526936e46349df8ac70
pengilton <94359705+pengilton@users.noreply.github.com>
2024-12-03 23:29:28 +0100

    Trying stuff

--- day/02/day_02.py
+++ day/02/day_02.py
@@ -20,8 +20,9 @@ def safe_check(report: list, min_diff: int, max_diff:int) -> bool:
     for i in range(len(report) - 1):
         num_a = report[i]
         num_b = report[i + 1]
+        diff = abs(num_a - num_b)
 
-        if (abs(num_a - num_b) < min_diff) or (max_diff < abs(num_a - num_b)):
+        if ((diff < min_diff) or (max_diff < diff)):
             return False
 
         if (num_a == num_b):
@@ -32,6 +33,64 @@ def safe_check(report: list, min_diff: int, max_diff:int) -> bool:
             is_increaasing = False
 
     return (is_increaasing or is_decreasing)
+
+
+def recursive_safe_check(report: list, min_diff: int, max_diff:int, tolerance: int) -> bool:
+    is_increaasing = True
+    is_decreasing = True
+    is_in_interval = True
+    safe = True
+
+    for i in range(len(report) - 1):
+        num_a = report[i]
+        num_b = report[i + 1]
+        diff = abs(num_a - num_b)
+        
+        if ((diff < min_diff) or (max_diff < diff)):
+            is_in_interval = False
+
+        if (num_a == num_b):
+            is_decreasing = False
+            is_increaasing = False
+        elif (num_a < num_b):
+            is_decreasing = False
+        else:
+            is_increaasing = False
+        
+        safe = (is_increaasing or is_decreasing) and is_in_interval
+
+        if (safe == False):
+            if tolerance > 0:
+                filtered_report = report.copy()
+                del filtered_report[i]
+                safe = recursive_safe_check(filtered_report, min_diff, max_diff, tolerance - 1)
+    
+    return safe
+
+
+def increasing_safe_check(report: list, min_diff: int, max_diff: int, tolerance: int) -> bool:
+    if len(report) < 2:
+        return True
+    
+    safe: bool = False
+    index: int = 1
+    while (index < len(report)):
+        # difference >= 0 because we assume that the list has increasing values
+        difference = report[index] - report[index - 1]
+
+        if (min_diff <= difference) and (difference <= max_diff):
+            index += 1
+        else:
+            if tolerance > 0:
+                filtered_report = report.copy()
+                del filtered_report[index - 1]
+
+                if increasing_safe_check(filtered_report, min_diff, max_diff, tolerance - 1):
+                    return True
+
+
+def decreaasing_safe_check(report: list, min_diff: int, max_diff: int, tolerance: int) -> bool:
+    return False
 
 
 def part_one():

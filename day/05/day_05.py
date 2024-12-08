678e5f25e4fa361d45633f9feb18a0375208731a
pengilton <94359705+pengilton@users.noreply.github.com>
2024-12-08 16:36:16 +0100

    Solved day 5

--- day/05/day_05.py
+++ day/05/day_05.py
@@ -53,13 +53,50 @@ def is_correct(update: list[int], rules: dict) -> bool:
     
     return True
 
-def filter_ordered_updates(updates: list[list[int]], rules: dict) -> list[int]:
+def filter_ordered_updates(updates: list[list[int]], rules: dict, return_unordered: bool = False) -> list[list[int]]:
     ordered_updates = []
+    unordered_updates = []
 
     for update in updates:
         if is_correct(update, rules):
             ordered_updates.append(update)
+        else:
+            unordered_updates.append(update)
 
+    if return_unordered:
+        return unordered_updates
+    else:
+        return ordered_updates
+
+
+# returnes ordred list according to rules
+def order_update(update: list[int], rules: dict) -> list[int]:
+    reversed_update = list(reversed(update))
+
+    ordered = False
+    while not ordered:
+        ordered = True
+        for i in range(len(reversed_update)):
+            # Number which will be checked
+            page = reversed_update[i]
+            for j in range(i + 1, len(reversed_update)):
+                # page has to be printed before this one
+                previous_page = reversed_update[j]
+                if previous_page in rules[page]:
+                    # If rule is broken, swap elements
+                    reversed_update[i], reversed_update[j] = reversed_update[j], reversed_update[i]
+                    ordered = False
+    
+    return list(reversed(reversed_update))
+
+
+def order_updates(unordred_updates: list[list[int]], rules: dict) -> list[list[int]]:
+    ordered_updates = []
+    
+    for update in unordred_updates:
+        ordered_update = order_update(update, rules)
+        ordered_updates.append(ordered_update)
+    
     return ordered_updates
 
 
@@ -82,9 +119,18 @@ def part_one():
 def part_two():
     file_name = "input_05.txt"
 
-    count = 0
+    input_list = file_reading(file_name)
+    rules_dict = create_rules(input_list)
+    updates = create_updates(input_list)
+    unordered_updates = filter_ordered_updates(updates, rules_dict, True)
+    ordered_updates = order_updates(unordered_updates, rules_dict)
     
-    print("X-MAS appears {} many times!".format(count))
+    count = 0
+    for update in ordered_updates:
+        middle = len(update) // 2
+        count += update[middle]
+    
+    print("Sum of all middle page numbers of the previously uncorrectly-ordered updates: {}".format(count))
 
 
 if __name__ == "__main__":

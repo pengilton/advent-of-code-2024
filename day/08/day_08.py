77b33f4fb0436d4dfb4aabe490441752f0f26542
pengilton <94359705+pengilton@users.noreply.github.com>
2024-12-22 23:05:34 +0100

    Day 8 part 2

--- day/08/day_08.py
+++ day/08/day_08.py
@@ -10,7 +10,7 @@ def read_file(file_name: str) -> np.ndarray:
     return np.array(input_2d)
 
 
-def get_antena_coordinates(antena_map: np.array, empty_spots: str = ".") -> list[tuple]:
+def get_antena_coordinates(antena_map: np.ndarray, empty_spots: str = ".") -> list[tuple]:
     coordinates = []
 
     rows = antena_map.shape[0]
@@ -34,7 +34,7 @@ def is_inside_map(antena_map: np.ndarray, coord: tuple) -> bool:
     return  (0 <= x) and (x < num_rows) and (0 <= y) and (y < num_cols)
 
 
-def get_antinode_coords(antena_map: np.array, firs_coord: tuple, second_cord: tuple) -> list[tuple]:
+def get_antinode_coords(antena_map: np.ndarray, firs_coord: tuple, second_cord: tuple) -> list[tuple]:
     valid_coords = []
 
     delta_coords = tuple(map(lambda x, y: x - y, second_cord, firs_coord))
@@ -50,7 +50,7 @@ def get_antinode_coords(antena_map: np.array, firs_coord: tuple, second_cord: tu
     return valid_coords
 
 
-def get_all_antinode_coords(antena_map: np.array, antena_coordinates: list[tuple]) -> set:
+def get_all_antinode_coords(antena_map: np.ndarray, antena_coordinates: list[tuple]) -> set:
     valid_coords = set()
 
     # We compare each antena with each other
@@ -73,7 +73,50 @@ def get_all_antinode_coords(antena_map: np.array, antena_coordinates: list[tuple
     return valid_coords
 
 
-def place_antinodes(antena_map: np.array, antena_coordinates: list[tuple], antinode: str = "#", empty_spot: str = "."):
+# Variant for part 2
+def get_antinode_coords_2(antena_map: np.ndarray, first_coord: tuple, second_cord: tuple, max_iter_count: int = 1) -> list[tuple]:
+    valid_coords = []
+
+    delta_coords = tuple(map(lambda x, y: x - y, second_cord, first_coord))
+
+    for k in range(max_iter_count + 1):
+        first_antinode = (first_coord[0] - k * delta_coords[0], first_coord[1] - k * delta_coords[1])
+        if is_inside_map(antena_map, first_antinode):
+            valid_coords.append(first_antinode)
+    
+    for k in range(max_iter_count + 1):
+        second_antinode = (second_cord[0] + k * delta_coords[0], second_cord[1] + k * delta_coords[1])
+        if is_inside_map(antena_map, second_antinode):
+            valid_coords.append(second_antinode)
+
+    return valid_coords
+
+# Variant for part 2
+def get_all_antinode_coords_2(antena_map: np.ndarray, antena_coordinates: list[tuple]) -> set:
+    valid_coords = set()
+    max_iter_count = max(antena_map.shape[0], antena_map.shape[1])
+
+    # We compare each antena with each other
+    for i in range(len(antena_coordinates) - 1):
+        first_coord: tuple = antena_coordinates[i]
+        for j in range(i + 1, len(antena_coordinates)):
+            second_coord: tuple = antena_coordinates[j]
+
+            first_antena = antena_map[first_coord[0]][first_coord[1]]
+            second_antena = antena_map[second_coord[0]][second_coord[1]]
+
+            # Only do stuff when both equel.
+            if first_antena == second_antena:
+                antinode_cords = get_antinode_coords_2(antena_map, first_coord, second_coord, max_iter_count)
+
+                # We check for each valid antinode coord if the spot is empty 
+                for antinode_cord in antinode_cords:
+                    valid_coords.add(antinode_cord)
+
+    return valid_coords
+
+
+def place_antinodes(antena_map: np.ndarray, antena_coordinates: list[tuple], antinode: str = "#", empty_spot: str = "."):
     # We compare each antena with each other
     for i in range(len(antena_coordinates) - 1):
         first_coord: tuple = antena_coordinates[i]
@@ -93,7 +136,7 @@ def place_antinodes(antena_map: np.array, antena_coordinates: list[tuple], antin
                         antena_map[antinode_cord[0]][antinode_cord[1]] = antinode
 
 
-def count_antinodes(antena_map: np.array, antinode: str = "#") -> int:
+def count_antinodes(antena_map: np.ndarray, antinode: str = "#") -> int:
     counter = 0
     for row in antena_map:
         for possible_antena in row:
@@ -115,9 +158,13 @@ def part_one():
 
 
 def part_two():
-    file_name = "test.txt"
+    file_name = "input_08.txt"
 
-    number_of_antinodes = 0
+    antena_map = read_file(file_name)
+    antena_coordinates = get_antena_coordinates(antena_map)
+    all_antinodes: set = get_all_antinode_coords_2(antena_map, antena_coordinates)
+
+    number_of_antinodes = len(all_antinodes)
 
     print("There are {} many anitnodes.".format(number_of_antinodes))
 
